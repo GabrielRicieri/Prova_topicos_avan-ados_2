e813e162b6064501f1e02ec510e25923bb9abaf7
GabrielRicieri <gabrielmartines145@gmail.com>
2021-07-01 15:46:22 -0300

    prova

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const productRoute = require('./routes/product.route');
+const customerRoute = require('./routes/customer.route');
+const saleRoute = require('./routes/sale.route');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Rodando');
+});
+
+app.use(express.json());
+app.use('/products', productRoute);
+app.use('/customers', customerRoute);
+app.use('/sales', saleRoute);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
