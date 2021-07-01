bd9738d47ba8be0ab9593810b223feccceea7a25
GabrielRicieri <gabrielmartines145@gmail.com>
2021-07-01 20:06:11 -0300

    prova

--- src/services.js
+++ src/services.js
@@ -1,33 +1,5 @@
 var NEW_DATA = [];
 
-const deleteBill = (req, res) => {
-  var id = `${req.params.id}`;
-  let valid = false;
-  for (let i = 0; i < NEW_DATA.length; i++) {
-    console.log(NEW_DATA[i].id);
-    console.log(id);
-    if (id == NEW_DATA[i].id) {
-      NEW_DATA.splice(i, 1);
-      valid = true;
-    }
-  }
-
-  if (!valid) res.json({ message: 'Bill not found' }, 404);
-  else res.json({}, 200);
-};
-
-const getBills = (req, res) => {
-  var customerR = req.query.customer;
-  var returnDataARRAY = [];
-  for (let i = 0; i < NEW_DATA.length; i++) {
-    if (customerR == NEW_DATA[i].customer) {
-      returnDataARRAY.push(NEW_DATA[i]);
-    }
-  }
-  if (returnDataARRAY == '') res.json({ message: 'Bill not found' }, 401);
-  else res.json({ result: returnDataARRAY }, 200);
-};
-
 const newBiils = (req, res) => {
   var body = req.body;
 
@@ -45,6 +17,34 @@ const newBiils = (req, res) => {
   };
   NEW_DATA.push(data);
   res.json({ message: 'Bill added with success', bill: data }, 200);
+};
+
+const getBills = (req, res) => {
+  var customerR = req.query.customer;
+  var returnDataARRAY = [];
+  for (let i = 0; i < NEW_DATA.length; i++) {
+    if (customerR == NEW_DATA[i].customer) {
+      returnDataARRAY.push(NEW_DATA[i]);
+    }
+  }
+  if (returnDataARRAY == '') res.json({ message: 'Bill not found' }, 401);
+  else res.json({ result: returnDataARRAY }, 200);
+};
+
+const deleteBill = (req, res) => {
+  var id = `${req.params.id}`;
+  let valid = false;
+  for (let i = 0; i < NEW_DATA.length; i++) {
+    console.log(NEW_DATA[i].id);
+    console.log(id);
+    if (id == NEW_DATA[i].id) {
+      NEW_DATA.splice(i, 1);
+      valid = true;
+    }
+  }
+
+  if (!valid) res.json({ message: 'Bill not found' }, 404);
+  else res.json({}, 200);
 };
 
 module.exports = {
